a24cfacbafdf46aac5268d0e8faf66648ca4fbe2
= <=>
2021-11-11 11:06:01 +0900

    testID 환경변수 설정

--- typecast_test.go
+++ typecast_test.go
@@ -3,12 +3,13 @@ package main_test
 import (
 	"fmt"
 	"testing"
+	"os"
 	."github.com/jipark0716/typecastGo"
 )
 
 func TestMain(t *testing.T) {
 	t.Error("1234")
-	typecast, _ := NewTypeCast("jipark0716@gmail.com", "h4CZ2gDgHrydinP")
+	typecast, _ := NewTypeCast(os.Getenv("TYPECAST_ID"), os.Getenv("TYPECAST_pw"))
 	for actor, _ := range Actor {
 		blob, err := typecast.Exec("ㅋㅋ루삥뽕", actor)
 		fmt.Printf("%s:%#v:%#v\n", actor, err, len(blob))
