1395c20e9aa13884bd2fa2f609cbd58531df86a6
jipark <jipark0716@gmail.com>
2022-10-06 11:32:13 +0900

    feat: oauth request response
    1. 로그인 구현

---
+++ endpoint.go
@@ -0,0 +1,18 @@
+package typecast
+
+var GoogleapiKey = "AIzaSyBJN3ZYdzTmjyQJ-9TdpikbsZDT9JUAYFk"
+var GoogleApiVersion = "3"
+var TypecastGid = "f8ON1ZpeF0mDFjZTQlr9G"
+
+var (
+	EndpointGoogleIdentitytoolkit = "https://www.googleapis.com/identitytoolkit/v" + GoogleApiVersion + "/relyingparty/"
+
+	EndpointGoogleSecurePassword = EndpointGoogleIdentitytoolkit + "verifyPassword?key=" + GoogleapiKey
+	EndpointGoogleSecureToken    = EndpointGoogleIdentitytoolkit + "verifyCustomToken?key=" + GoogleapiKey
+
+	EndpointTypecastApi = "https://typecast.ai/api/"
+
+	EndpointTypecastOauth     = EndpointTypecastApi + "auth-fb/custom-token"
+	EndpointTypecastSpeakPost = EndpointTypecastApi + "speak/batch/post"
+	EndpointTypecastSpeakGet  = EndpointTypecastApi + "speak/batch/get"
+)
--- typecast_test.go
+++ typecast_test.go
@@ -1,20 +1,14 @@
-package typecast_test
+package typecast
 
 import (
-	"fmt"
 	"testing"
-	"os"
-	."github.com/jipark0716/typecastGo"
 )
 
 func TestMain(t *testing.T) {
-	t.Error("1234")
-	typecast, _ := NewTypeCast(os.Getenv("TYPECAST_ID"), os.Getenv("TYPECAST_pw"))
-	for actor, _ := range Actor {
-		go func(actor string) {
-			blob, err := typecast.Exec("ㅋㅋ루삥뽕", actor)
-			fmt.Printf("%s:%#v:%#v\n", actor, err, len(blob))
-		}(actor)
-	}
-	select{}
+	typecast := NewSession()
+	typecast.Connect(&LoginRequest{
+		Email:             "",
+		Password:          "",
+		ReturnSecureToken: true,
+	})
 }
